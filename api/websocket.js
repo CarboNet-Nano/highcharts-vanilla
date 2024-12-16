57ef767c1b1d922c911813a5cb9ded24bcc8a76c
bschonbrun <bill@carbonet.com>
2024-12-16 11:24:59 +0800

    Update websocket.js

--- api/websocket.js
+++ api/websocket.js
@@ -1,37 +1,62 @@
-const { WebSocket, WebSocketServer } = require('ws');
+<function_calls>
+<invoke name="artifacts">
+<parameter name="id">websocket-debug</parameter>
+<parameter name="type">application/vnd.ant.code</parameter>
+<parameter name="command">create</parameter>
+<parameter name="content">const { WebSocketServer } = require('ws');
 
+let wss;
 const connections = new Map();
 
 exports.handler = async (event, context) => {
-  if (event.headers.upgrade !== 'websocket') {
+  // Log the incoming request
+  console.log('WebSocket handler called:', {
+    httpMethod: event.httpMethod,
+    headers: event.headers
+  });
+
+  // Check for WebSocket upgrade
+  if (event.requestContext.routeKey === '$connect') {
+    console.log('WebSocket connection attempt');
+    
+    if (!wss) {
+      wss = new WebSocketServer({ 
+        clientTracking: true,
+        noServer: true 
+      });
+
+      wss.on('connection', (ws) => {
+        console.log('New client connected');
+        const id = Math.random().toString(36).substring(7);
+        connections.set(id, ws);
+
+        ws.on('close', () => {
+          console.log('Client disconnected:', id);
+          connections.delete(id);
+        });
+      });
+    }
+
     return {
-      statusCode: 400,
-      body: 'WebSocket connections only'
+      statusCode: 200,
+      body: 'Connected'
     };
   }
 
-  const wss = new WebSocketServer({ noServer: true });
-  
-  wss.on('connection', (ws) => {
-    const id = Math.random().toString(36).substring(7);
-    connections.set(id, ws);
-
-    ws.on('close', () => {
-      connections.delete(id);
-    });
-  });
-
   return {
-    statusCode: 200,
-    body: 'Connected'
+    statusCode: 400,
+    body: 'Invalid request'
   };
 };
 
-// Function to broadcast updates to all connected clients
+// Function to broadcast updates
 exports.broadcast = (data) => {
+  console.log('Broadcasting to', connections.size, 'clients:', data);
   connections.forEach((ws) => {
     if (ws.readyState === WebSocket.OPEN) {
       ws.send(JSON.stringify(data));
     }
   });
-};
+};</parameter>
+<parameter name="language">javascript</parameter>
+</invoke>​​​​​​​​​​​​​​​​
